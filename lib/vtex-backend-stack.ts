ebdb27e7d622ef696519a3adc58333025bdf1293
Michelly Teixeira <michelly.teixeira@vtex.com.br>
2023-06-02 18:13:33 -0300

    add initial handler for payment lambda

--- lib/vtex-backend-stack.ts
+++ lib/vtex-backend-stack.ts
@@ -3,8 +3,35 @@ import { LambdaIntegration, RestApi, Cors } from 'aws-cdk-lib/aws-apigateway';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as path from 'path';
+import * as DynamoDB from "aws-cdk-lib/aws-dynamodb";
+import * as cdk from "aws-cdk-lib";
+import * as sqs from "aws-cdk-lib/aws-sqs";
 
 export const configureBackend = (scope: any) => {
+  const AcquirerAPI = "testing-12345/v2"
+
+  // Create a DynamoDB table called PaymentTrackTable
+  const PaymentTrackTable = new DynamoDB.Table(scope, "PaymentTrackTable", {
+    partitionKey: {
+      name: "paymentId",
+      type: DynamoDB.AttributeType.STRING,
+    },
+    tableName: "PaymentTrackTable",
+    billingMode: DynamoDB.BillingMode.PAY_PER_REQUEST,
+    stream: DynamoDB.StreamViewType.NEW_IMAGE,
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+  });
+
+  /////// QUEUE
+
+  // create an SQS queue called PendingPaymentQueue
+  const PendingPaymentQueue = new sqs.Queue(scope, "PendingPaymentQueue", {
+    queueName: "PendingPaymentQueue",
+    visibilityTimeout: cdk.Duration.seconds(30),
+    receiveMessageWaitTime: cdk.Duration.seconds(10),
+    retentionPeriod: cdk.Duration.days(1),
+    encryption: sqs.QueueEncryption.SQS_MANAGED,
+  });
 
   // Create an API Gateway using Lambda Proxy
   const PaymentApi = new RestApi(scope, "PaymentApi", {
@@ -37,7 +64,7 @@ export const configureBackend = (scope: any) => {
   ManifestResource.addMethod("GET", ManifestLambdaIntegration);
 
   // Create UserPost Lambda function
-  const User = new NodejsFunction(scope, "User", {
+  const UserApi = new NodejsFunction(scope, "User", {
     memorySize: 128,
     timeout: Duration.seconds(5),
     runtime: Runtime.NODEJS_16_X,
@@ -45,11 +72,14 @@ export const configureBackend = (scope: any) => {
     entry: path.join(__dirname, "/../src/backend-front/User/handler.ts"),
     bundling: {
       minify: true
-    }
+    },
+    environment: {
+      TABLE_NAME: PaymentTrackTable.tableName
+    },
   });
 
   // create Lambda Proxy Integration and resource, and add integration to api resource
-  const UserApiLambdaIntegration = new LambdaIntegration(User);
+  const UserApiLambdaIntegration = new LambdaIntegration(UserApi);
   const UserApiResource = PaymentApi.root.addResource("user");
   UserApiResource.addMethod("POST", UserApiLambdaIntegration);
 }
--- src/backend-front/User/handler.ts
+++ src/backend-front/User/handler.ts
@@ -8,12 +8,12 @@ Responsible for registering new users in VTEX and acquirers
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
 
   return {
-    body: JSON.stringify('Hello from Lambda'),
+    body: JSON.stringify('Hello from Lambda!'),
+    statusCode: 200,
     headers: {
       'Access-Control-Allow-Headers': 'Content-Type',
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+      'Access-Control-Allow-Methods': 'POST'
     },
-    statusCode: 200
   };
 };
