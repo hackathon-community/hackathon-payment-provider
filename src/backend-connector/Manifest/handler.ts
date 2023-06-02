eb07f27326b58c90cc56efd4ea9193775c3366a9
Rafael Weffort <rafaelweffort@gmail.com>
2023-06-02 18:42:09 -0300

    fixing cors

--- src/backend-connector/Manifest/handler.ts
+++ src/backend-connector/Manifest/handler.ts
@@ -10,6 +10,11 @@ export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGateway
 
   return {
     body: JSON.stringify(manifest),
-    statusCode: 200
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+    },
   };
 };
